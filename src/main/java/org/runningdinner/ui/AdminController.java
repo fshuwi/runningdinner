a33436c5d9f055c15c78b375878327844f251a1c
clemens <clemensstich@web.de>
2014-02-27 17:18:43 +0100

    openshift bug logging commented out

--- src/main/java/org/runningdinner/ui/AdminController.java
+++ src/main/java/org/runningdinner/ui/AdminController.java
@@ -58,7 +58,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.web.servlet.support.RequestContextUtils;
 
 /**
  * Provides all methods for managing a created running dinner.<br>
@@ -298,12 +297,12 @@ public class AdminController extends AbstractBaseController {
 		model.addAttribute("uuid", uuid);
 
 		// *** Test for OPENSHIFT *** //
-		Map<String, ?> map = RequestContextUtils.getInputFlashMap(request);
-		LOGGER.warn("Receive flash map with {} elements", (map != null ? map.size() : 0));
-		if (map != null) {
-			Object statusMessage = map.get("statusMessage");
-			LOGGER.warn("Received statusMessage attribute {}", statusMessage);
-		}
+		// Map<String, ?> map = RequestContextUtils.getInputFlashMap(request);
+		// LOGGER.warn("Receive flash map with {} elements", (map != null ? map.size() : 0));
+		// if (map != null) {
+		// Object statusMessage = map.get("statusMessage");
+		// LOGGER.warn("Received statusMessage attribute {}", statusMessage);
+		// }
 		// *** END Test for Openshift *** //
 
 		return getFullViewName("editMealTimesForm");
