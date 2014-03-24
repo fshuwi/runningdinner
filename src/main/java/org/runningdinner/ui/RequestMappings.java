7fb46e33dd879388faa67c61bdbaa68d9f0c0f7f
clemens <clemensstich@web.de>
2014-03-24 22:30:33 +0100

    - changed export url to spring excel view url pattern

--- src/main/java/org/runningdinner/ui/RequestMappings.java
+++ src/main/java/org/runningdinner/ui/RequestMappings.java
@@ -9,7 +9,7 @@ public class RequestMappings {
 	public static final String ADMIN_OVERVIEW = ADMIN_URL_PATTERN;
 	public static final String SHOW_TEAMS = ADMIN_URL_PATTERN + "/teams";
 	public static final String SEND_TEAM_MAILS = ADMIN_URL_PATTERN + "/teams/mail";
-	public static final String EXPORT_TEAMS = ADMIN_URL_PATTERN + "/teams/export";
+	public static final String EXPORT_TEAMS = SHOW_TEAMS + ".xls"; // Use Spring content negotiation
 	public static final String SEND_PARTICIPANT_MAILS = ADMIN_URL_PATTERN + "/participants/mail";
 	public static final String SEND_DINNERROUTES_MAIL = ADMIN_URL_PATTERN + "/dinnerroute/mail";
 	public static final String SHOW_PARTICIPANTS = ADMIN_URL_PATTERN + "/participants";
